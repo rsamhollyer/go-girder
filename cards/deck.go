d95b041e2d0eba489a35ac03d353c5373879fd1d
Sam Hollyer <rsamhollyer@gmail.com>
2022-10-30 11:20:10 -0500

    updates

--- cards/deck.go
+++ cards/deck.go
@@ -10,3 +10,18 @@ func (d deck) print() {
 		fmt.Println(i, card)
 	}
 }
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
