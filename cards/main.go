00c1e2e51d91f08e8d3c0a0293150e76bfcff12b
Sam Hollyer <rsamhollyer@gmail.com>
2022-10-30 11:21:02 -0500

    updates

--- cards/main.go
+++ cards/main.go
@@ -2,13 +2,8 @@ package main
 
 func main() {
 
-	cards := deck{newCard(), "Ace of Spades"}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
 
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
